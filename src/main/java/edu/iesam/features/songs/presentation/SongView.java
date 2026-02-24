fe4f11b8dfccb5da3b12b1a74295e653f8f8d449
Angel <angel.sanzam@educa.jcyl.es>
2026-02-24 21:36:15 +0100

    Ejercicio 4a: Creada funcionalidad para añadir una unica instancia de memoria en songs.

--- src/main/java/edu/iesam/features/songs/presentation/SongView.java
+++ src/main/java/edu/iesam/features/songs/presentation/SongView.java
@@ -1,6 +1,7 @@
 package edu.iesam.features.songs.presentation;
 
-import edu.iesam.features.songs.data.SongBdDataRepository;
+import edu.iesam.features.songs.data.SongDataRepository;
+import edu.iesam.features.songs.data.SongMemLocalDataSource;
 import edu.iesam.features.songs.domain.GetSongsUseCase;
 import edu.iesam.features.songs.domain.Song;
 
@@ -10,7 +11,7 @@ public class SongView {
 
 
     public static void printSongs(){
-        GetSongsUseCase getSongsUseCase = new GetSongsUseCase(new SongBdDataRepository());
+        GetSongsUseCase getSongsUseCase = new GetSongsUseCase(new SongDataRepository(SongMemLocalDataSource.newInstance()));
 
         ArrayList<Song> songsList = getSongsUseCase.execute();
 
